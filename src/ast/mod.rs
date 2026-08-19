77b79f308c15f40f344acb98e815a1c196f09987
namtran1812 <158846154+namtran1812@users.noreply.github.com>
2026-08-19 12:56:57 -0400

    feat: add lexer parser and typed AST

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,1 +1,29 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Integer(i64),
+    Identifier(String),
+    Binary {
+        left: Box<Expr>,
+        op: BinaryOp,
+        right: Box<Expr>,
+    },
+}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BinaryOp {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Statement {
+    Let { name: String, value: Expr },
+    Return(Expr),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Program {
+    pub statements: Vec<Statement>,
+}
